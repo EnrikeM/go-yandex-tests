71527eba0ca0d87d28379912e556f549fe1a4568
Enrike_personal <enrike_personal@MacBook-Pro.local>
2023-12-31 14:23:39 +0700

    iter1 finished

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,3 +1,23 @@
 package main
 
-func main() {}
+import (
+	"fmt"
+	"net/http"
+	"os"
+
+	"github.com/EnrikeM/go-yandex-tests/internal/app/handlers"
+	"github.com/EnrikeM/go-yandex-tests/internal/storage"
+	"github.com/EnrikeM/go-yandex-tests/internal/storage/common"
+	"github.com/EnrikeM/go-yandex-tests/internal/storage/inmem"
+)
+
+func main() {
+	im := common.Storage(inmem.New())
+	storage.SetUsed(&im)
+
+	http.HandleFunc(handlers.ShortenPath, handlers.HandlerFunc)
+	if err := http.ListenAndServe(":8080", http.HandlerFunc(handlers.HandlerFunc)); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
